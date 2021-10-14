9f4170081be994f92846435c234f2f2bdb86d6c7
betamoreira <79289691+betamoreira@users.noreply.github.com>
2021-10-14 16:17:34 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,21 +3,23 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var caixa1;
+var caixa1, solo;
 
 function setup(){
     var canvas = createCanvas(400,400);
     engine = Engine.create();
     world = engine.world;
 
-    caixa1 = new Box(200,100,50,50);
-    
+    box1 = new Box(200,300,50,50);
+    box2 = new Box(240,100,50,100);
+    ground = new Ground(200,height,400,20)
 }
 
 function draw(){
     background(0);
     Engine.update(engine);
-
-    caixa1.display();
     
+    box1.display();
+    box2.display();
+    ground.display();
 }
